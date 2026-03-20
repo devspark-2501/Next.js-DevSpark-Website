630537a7c6c2df8d94cf1b758431a872ef827ee0
Tanush Mathur <devspark2501@gmail.com>
2026-03-20 16:00:25 +0530

    Update in mongoDB

---
+++ app/(DB)/UpdateDB.mongodb.js
@@ -0,0 +1,16 @@
+use('ecommerce');
+
+// db.products.updateOne(
+// { name: "Wireless Mouse" },
+// { $set: { price: 4000 } }
+// ) 
+// updates mouse price or price of selected query!!
+
+
+// db.products.updateMany(
+//     { category: "Electronics" },
+//     { $set: { price: 5000 }}
+// ) 
+// updates price of every electronic category product!!
+
+
